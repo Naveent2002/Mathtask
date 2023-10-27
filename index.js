7f662946832865aeb0a6d94b3a8595bdbfee094d
naveen <146159067+Naveent2002@users.noreply.github.com>
2023-10-27 21:57:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var firstNum = Math.ceil(Math.random() * 10);
-var secondNum = Math.floor(Math.random() * 10) + 1;
-document.getElementById("fNum").value = firstNum;
-document.getElementById("sNum").value = secondNum;
- function checkSum(){  
-var ans = parseInt(document.getElementById("ansBox").value);
-            if (firstNum + secondNum === ans) {
-                document.getElementById("answer").innerHTML = "Correct";
-            } else {
-                document.getElementById("answer").innerHTML = "Incorrect";
-            }
-        }
-    function reset(){
-        
-    var firstNum=document.getElementById("fNum").value="";
-    var firstNum=document.getElementById("sNum").value="";
-    var ans=document.getElementById("ansBox").value="";
-    }
+
